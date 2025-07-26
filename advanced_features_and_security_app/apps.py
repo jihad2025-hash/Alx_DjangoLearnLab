2a1c61767a5085bb701cb5d494a3f34cf1f14bc9
gehad513 <gehadhas489@gmail.com>
2025-07-27 00:03:56 +0300

    Add new app advanced_features_and_security_app

---
+++ advanced_features_and_security_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdvancedFeaturesAndSecurityAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'advanced_features_and_security_app'
